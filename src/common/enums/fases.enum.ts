7e975b4e9c28765dfa3b88b6cea1104061079a5a
Sergio <ptsergiorivera@gmail.com>
2025-09-16 09:28:06 -0300

    enums fases

--- src/common/enums/fases.enum.ts
+++ src/common/enums/fases.enum.ts
@@ -1,5 +1,5 @@
 export enum FasesEnum {
-  FASE_1 = 'Fase 1',
-  FASE_2 = 'Fase 2',
-  FASE_3 = 'Fase 3',
+  FASE_1 = 'Fase 1 Jogo de Dentro',
+  FASE_2 = 'Fase 2 Jogo de Fora',
+  FASE_3 = 'Fase 3 Jogo Ligero',
 }
